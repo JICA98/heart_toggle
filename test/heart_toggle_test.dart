45d7e4ab9500622d64c4b14a47f6f7d972af091d
jica98 <jica.etzc@gmail.com>
2022-02-21 18:26:11 +0530

    first commit

---
+++ test/heart_toggle_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:heart_toggle/heart_toggle.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+}
